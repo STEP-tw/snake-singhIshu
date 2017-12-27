8fd82240301f1820fa054558d53479c3b005d217
Ishu <ishus1242@gmail.com>
2017-12-27 13:27:09 +0530

    Saving work before going to lunch

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ const animateSnake=function() {
   let oldHead=snake.getHead();
   let oldTail=snake.move();
   let head=snake.getHead();
+  endGame(head);
   paintBody(oldHead);
   unpaintSnake(oldTail);
   paintHead(head);
@@ -17,6 +18,46 @@ const animateSnake=function() {
     createFood(numberOfRows,numberOfCols);
     drawFood(food);
   }
+}
+
+const isNorthOrWestBrdr = function(coordX,coordY) {
+  return coordX < 0 || coordY < 0;
+}
+
+const isSouthOrEastBrdr = function(coordX,coordY) {
+  return coordX > 120 || coordY > 60 ;
+}
+
+const hasTouchedBorder = function(head) {
+  let coordX = head.getCoord()[0];
+  let coordY = head.getCoord()[1];
+  return isSouthOrEastBrdr(coordX,coordY) || isNorthOrWestBrdr(coordX,coordY);
+}
+
+const getCoordOfBody = function(body) {
+  return body.map(function(position) {
+    return {x:position.x,y:position.y};
+  })
+}
+
+const hasTouchedItself = function(head,body) {
+
+
+}
+
+const displayResult = function() {
+  let result = document.getElementById("gameOver");
+  let playAgainLink = document.getElementById("playAgain");
+  result.innerHTML= "Game Over";
+  playAgainLink.innerHTML = "Click here to Play Again";
+}
+
+const endGame= function(head) {
+  if (hasTouchedBorder(head)) {
+    clearInterval(animator);
+    displayResult();
+  }
+  return;
 }
 
 const changeSnakeDirection=function(event) {
